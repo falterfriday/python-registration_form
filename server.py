4552488a14a563d190c0ea5a3ba46e4b35e67a77
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-12 15:51:04 -0400

    modified server.py to validate form data for email, email format, first and last name, password length and match

---
+++ server.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, redirect, request, session, flash
+
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]*$')
+PASSWORD_REGEX = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$')
+
+
+app = Flask(__name__)
+
+app.secret_key = "ihuvlbln;kq3ijop34ty89ergiohdfbm"
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def submit():
+	if len(request.form['email']) < 1:
+		flash("email required")
+	elif not EMAIL_REGEX.match(request.form['email']):
+		flash("invalid email address!")
+	else:
+		flash("email success!")
+	if len(request.form['f_name']) < 1:
+		flash("first name required")
+	elif not NAME_REGEX.match(request.form['f_name']):
+		flash("invalid first name")
+	else:
+		flash("first name success!")
+	if len(request.form['l_name']) < 1:
+		flash("last name required")
+	elif not NAME_REGEX.match(request.form['l_name']):
+		flash("invalid last name")
+	else:
+		flash("last name success!")
+	if len(request.form['password']) < 8:
+		flash("longer password1 required")
+	elif not PASSWORD_REGEX.match(request.form['password']):
+		flash("invalid password")
+	else:
+		flash("password success!")
+	if request.form['password'] != request.form['conf_password']:
+		flash("passwords dont match")
+	else:
+		flash("passwords match!")
+	return redirect('/')
+
+
+app.run(debug=True)
